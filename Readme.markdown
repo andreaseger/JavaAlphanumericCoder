8e2e528cf3ac83a4af79e686deb72af4027f2800
sch1zo <andreas.eger.reg@googlemail.com>
2011-02-17 00:38:19 +0100

    just a little change to test something
    Signed-off-by: sch1zo <andreas.eger.reg@googlemail.com>

--- Readme.markdown
+++ Readme.markdown
@@ -3,9 +3,8 @@ Alphanumeric Coder
 
 just a little java app to generate a few million alphanumeric codes and write them into a csv file.
 
-surprisingly this approach is slower than the original ruby+redis version
+either extreme slow but certainly unique through a HashSet or damn fast but not 100% sure that all codes unique
 
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a>
 
 Eger Andreas 2011
-
--- src/Coder.java
+++ src/Coder.java
@@ -1,4 +1,5 @@
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -31,13 +32,31 @@ public class Coder {
 		     }
 		     writer.append('\n');
 		  }
+			writer.flush();
+			writer.close();
 		}
 		catch(Exception e)
 		{
 		  e.printStackTrace();
 		}
 	}
-	
+	private static void generate_and_save(int count, int per_row){
+		try {
+			FileWriter writer = new FileWriter("codes.csv");
+			for(int i = 0;i<count;i++){
+				writer.append(RandomStringUtils.randomAlphanumeric(10));
+				if(i%per_row == 0)
+					writer.append('\n');
+				else
+					writer.append(',');
+			}
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	/**
 	 * @param args
 	 */
@@ -59,7 +78,8 @@ public class Coder {
 		}catch (NumberFormatException  e) {
 			e.printStackTrace();
 		}
-		generate(count);
-		write_to_csv(per_row);
+		//generate(count);
+		//write_to_csv(per_row);
+		generate_and_save(count, per_row);
 	}
 }
